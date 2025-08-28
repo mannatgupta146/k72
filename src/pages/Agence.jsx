6ce252f08fbb62d00e3b944251ed66ee5ef911c1
mannatgupta146 <mannatgupta146@gmail.com>
2025-08-28 23:06:43 +0530

    added

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Agence = () => {
   return (
-    <div>
-      
+    <div className='font-[font2] '>
+      <div className='mt-[55vh]'>
+        <h1 className="text-[18vw] uppercase leading-[17vw] text-center" > SEVEN7Y <br /> TWO</h1>
+      </div>
+      <div className='pl-[40%] mt-20'>
+        <p className='text-5xl'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+          We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. 
+          A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, 
+          but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
+      </div>
     </div>
   )
 }
