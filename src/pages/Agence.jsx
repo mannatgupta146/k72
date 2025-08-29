2cf1b7010cc717393d97760a172759f274f92073
mannatgupta146 <mannatgupta146@gmail.com>
2025-08-29 21:49:32 +0530

    added

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Agence = () => {
   return (
-    <div className='font-[font2] '>
+    <div className='section1'>
+      <div className='h-[20vw] w-[15vw] rounded-4xl overflow-hidden absolute top-36 left-[31vw]'>
+        <img className='h-full w-full object-cover' src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt="" />
+      </div>
+
+      <div className='relative font-[font2] '>
       <div className='mt-[55vh]'>
         <h1 className="text-[18vw] uppercase leading-[17vw] text-center" > SEVEN7Y <br /> TWO</h1>
       </div>
@@ -11,6 +16,7 @@ const Agence = () => {
           We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. 
           A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, 
           but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
+      </div>
       </div>
     </div>
   )
