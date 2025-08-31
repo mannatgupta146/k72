ecc3c7569c0cc344a31e873bf041d862272e0f84
mannatgupta146 <mannatgupta146@gmail.com>
2025-09-01 00:23:10 +0530

    fixed error

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const FullScreenNav = () => {
   return (
     <div>
-      
+      <div>
+        <div>
+          <h1>Projects</h1>
+        </div>
+      </div>
     </div>
   )
 }
