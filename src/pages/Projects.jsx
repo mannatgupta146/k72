8d9ea482e902df5f1f22e2d88e06aee740efa9b0
mannatgupta146 <mannatgupta146@gmail.com>
2025-09-02 20:21:09 +0530

    projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,25 +1,56 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react'
+import ProjectCard from '../components/projects/ProjectCard'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
 
 const Projects = () => {
+
+  const projects = [{
+    image1: 'https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg'
+  }, {
+    image1: 'https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/Opto/thumbnailimage_opto-1280x960.jpg'
+  }, {
+    image1: 'https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg'
+  }]
+
+
+  gsap.registerPlugin(ScrollTrigger)
+
+  useGSAP(function () {
+    gsap.from('.hero', {
+      height: '100px',
+      stagger: {
+        amount: 0.4
+      },
+      scrollTrigger: {
+        trigger: '.lol',
+        start: 'top 100%',
+        end: 'top -150%',
+        scrub: true
+      }
+    })
+  })
+
   return (
-    <div className='p-4'>
-      <div className='pt-[40vh]'>
-        <h2 className='font-[font2] text-[10vw] uppercase'>Work</h2>
+    <div className='lg:p-4 p-2 mb-[100vh]'>
+      <div className=' flex pt-[45vh]'>
+        <h2 className='font-[font2] lg:text-[10vw] text-7xl uppercase'>Work</h2>
+        <h4 className='font-[font2] pl-2 pt-1 text-2xl'>16</h4>
       </div>
-      <div>
-        <div className='-mt-10'>
-          <div className='w-full h-[700px] mb-4 flex gap-4-mt-10'>
-          <div className='w-1/2 rounded-none transition-all hover:rounded-[50px] overflow-hidden h-full'>
-          <img className='w-full h-full object-cover' src="" alt="" />
-          <div className='w-full h-full absolute top-0 left-0 bg-black/10'></div>
+      <div className='-lg:mt-20 lol'>
+        {projects.map(function (elem, idx) {
+          return <div key={idx} className='hero w-full lg:h-[550px] mb-4 flex lg:flex-row flex-col lg:gap-4 gap-2'>
+            <ProjectCard image1={elem.image1} image2={elem.image2} />
           </div>
-          
-        </div>
-        </div>
+        })}
 
       </div>
     </div>
   )
-}
+} 
 
 export default Projects
