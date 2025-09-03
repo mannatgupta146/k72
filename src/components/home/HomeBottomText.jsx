86899b6bac4fd9a529e032d0aa10b402ae4b002f
mannatgupta146 <mannatgupta146@gmail.com>
2025-09-03 14:53:35 +0530

    making responsive

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -4,20 +4,20 @@ import { Link, useNavigate } from 'react-router-dom'
 const HomeBottomText = () => {
 
   return (
-    <div className='font-[font2] flex items-center justify-center gap-5'>
+    <div className='font-[font2] flex items-center justify-center lg:gap-7 gap-3'>
 
-      <p className='absolute lg:w-[17vw] w-64 lg:right-6 right-0 bottom-28  lg:bottom-29 font-[font1] lg:text-lg text-xs lg:leading-relaxed leading-tight'>
+      <p className='absolute lg:w-[17vw] w-67 lg:right-6 right-0 bottom-30  lg:bottom-29 font-[font1] lg:text-lg text-1xl lg:leading-relaxed leading-tight'>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
        K72 is an agency that builds brands from every angle. 
        Today, tomorrow and years from now. We think the best sparks fly when comfort zones get left behind and friction infuses our strategies, 
        brands and communications with real feeling. We’re transparent, honest and say what we mean, and when we believe in something, we’re all in.</p>
       
-      <div className='lg:border-3 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] lg:h-34 flex items-center px-3 pt-1 lg:px-14 border-white rounded-full uppercase'>
-        <Link className='text-[5vw] lg:mt-3' to='/projects'>Projects</Link>
+      <div className='lg:border-4 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] lg:h-30 flex items-center px-5 pt-1 lg:px-10 border-white rounded-full uppercase'>
+        <Link className='lg:text-[6vw] text-[7vw] lg:mt-3' to='/projects'>Work</Link>
       </div>
       
-      <div className='lg:border-3 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50]  lg:h-34 flex items-center px-3 pt-1 lg:px-14 border-white rounded-full uppercase'>
-        <Link className='text-[5vw] lg:mt-3' to='/agence'>Agence</Link>
+      <div className='lg:border-4 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50]  lg:h-30 flex items-center px-5 pt-1 lg:px-10 border-white rounded-full uppercase'>
+        <Link className='lg:text-[6vw] text-[7vw] lg:mt-3' to='/agence'>Agency</Link>
       </div>
     </div>
   )
--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -3,18 +3,18 @@ import Video from './Video'
 
 const HomeHeroText = () => {
     return (
-        <div className='font-[font2] pt-5 text-center'>
-            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[10vw]'>
+        <div className='font-[font2] pt-5 lg:mt-0 mt-20 text-center'>
+            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[11vw]'>
                 The spark for
             </div>
-            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-start uppercase lg:leading-[8vw] leading-[10vw]'>
+            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-start uppercase lg:leading-[8vw] leading-[11vw]'>
                 all
-                <div className='h-[8vw] w-[16vw] rounded-full -mt-2 overflow-hidden'>
+                <div className='lg:h-[8vw] lg:w-[16vw] h-[9vw] w-[18vw] rounded-full lg:-mt-2 overflow-hidden'>
                     <Video />
                 </div>
                 things
             </div>
-            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[10vw]'>
+            <div className='lg:text-[9.5vw] text-[12vw] justify-center flex items-center uppercase lg:leading-[8vw] leading-[11vw]'>
                 creative
             </div>
         </div>
